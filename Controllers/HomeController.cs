9603f14fcfef59cbe84d29d3bece3cbd94440733
MatousAc <matoush@southern.edu>
2021-04-30 18:13:58 -0400

    try get js working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Handlers;
+using System.Threading.Tasks;
 
 namespace blender.Controllers
 {
     public class HomeController : Controller
     {
+        public string systemToggle()
+        {
+            return "metric";
+        }
         public ActionResult Index()
         {
+
             return View();
         }
 
--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,15 @@
 ﻿// javascript that the whole app can use
 
-dq = function (selector) { return document.querySelector(selector)}
+dq = function (selector) { return document.querySelector(selector) }
+sg = function (item) { return sessionStorage.getItem(item) }
+ss = function (item, val) { return sessionStorage.setItem(item, val) }
+
+toggleSystem = function () {
+    if ((sg("system") == "imperial") || (sg("system") == null)) {
+        ss("system", "metric")
+    }
+    else {
+        ss("system", "imperial")
+    }
+    location.reload()
+}
